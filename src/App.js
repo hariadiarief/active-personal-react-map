1a1cc38964ba55f085a20fb2a18bac4b718e2b72
hariadiarief <adi.arief77@gmail.com>
2020-04-11 17:30:02 +0700

    implement react location picker

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
 import Layout from './layout'
 import Map from './Page/Map'
 
 function App() {
 	return (
-		<Layout>
-			<Map />
-		</Layout>
+		<Router>
+			<Switch>
+				<Layout>
+					<Route path='/' exact component={Map} />
+				</Layout>
+			</Switch>
+		</Router>
 	)
 }
 
--- src/Page/Map.js
+++ src/Page/Map.js
@@ -6,6 +6,8 @@ import { ReactComponent as IconGps } from '../images/banner/gps.svg'
 import InputAddress from '../components/InputAddress'
 import InputCoordinat from '../components/InputCoordinat'
 
+import LocationPicker from 'react-location-picker'
+
 const SEARCH_BY_NAME = 1
 const SEARCH_BY_COORDINAT = 2
 
@@ -14,9 +16,37 @@ class Map extends Component {
 		super(props)
 		this.state = {
 			whichInput: SEARCH_BY_NAME,
+			address: 'Kala Pattar Ascent Trail, Khumjung 56000, Nepal',
+			position: {
+				lat: 0,
+				lng: 0,
+			},
 		}
 	}
+
+	componentDidMount() {
+		this.locationInitiation()
+	}
+
+	locationInitiation() {
+		window.navigator.geolocation.getCurrentPosition(
+			(position) =>
+				this.setState({
+					position: {
+						lat: position.coords.latitude,
+						lng: position.coords.longitude,
+					},
+				}),
+			(err) => this.setState({ errorMessage: err.message })
+		)
+	}
+
+	handleLocationChange = ({ position, address }) => {
+		this.setState({ position, address })
+	}
+
 	render() {
+		const { whichInput, position, address } = this.state
 		return (
 			<div className='map'>
 				<div className='map__banner'>
@@ -25,36 +55,49 @@ class Map extends Component {
 				</div>
 				<div className='map__display'>
 					<div className='navigation map__display__navigation'>
-						<div className='navigation map__display__navigation'>
-							<div className='navigations'>
-								<a className={`navigation ${whichInput === SEARCH_BY_NAME && 'navigation--active'}`} onClick={() => this.setState({ whichInput: SEARCH_BY_NAME })}>
-									Pilih Berdasarkan Nama
-								</a>
-								<a
-									className={`navigation ${whichInput === SEARCH_BY_COORDINAT && 'navigation--active'}`}
-									onClick={() => this.setState({ whichInput: SEARCH_BY_COORDINAT })}>
-									Pilih Berdasarkan Koordinat
-								</a>
-							</div>
-						</div>
-						{/* <div className='navigations'>
-							<NavLink className='navigation' activeClassName='navigation--active' exact to={`/`}>
+						<div className='navigations'>
+							<a className={`navigation ${whichInput === SEARCH_BY_NAME && 'navigation--active'}`} onClick={() => this.setState({ whichInput: SEARCH_BY_NAME })}>
 								Pilih Berdasarkan Nama
-							</NavLink>
-							<NavLink className='navigation' activeClassName='navigation--active' exact to={`/`}>
+							</a>
+							<a className={`navigation ${whichInput === SEARCH_BY_COORDINAT && 'navigation--active'}`} onClick={() => this.setState({ whichInput: SEARCH_BY_COORDINAT })}>
 								Pilih Berdasarkan Koordinat
-							</NavLink>
+							</a>
 						</div>
-
-						<Switch>
-							<Route path={`/merchant/doofit/update/:id/user`} exact component={InputAddress} />
-							<Route path='/merchant/doofit/update/:id/busines' exact component={InputCoordinat} />
-							<Route path='/merchant/doofit/update/:id/gallery' exact component={Gallery} />
-						</Switch> */}
+						{this.renderWhichSearchingType()}
+						<div>
+							<span>Address : {address}</span>
+							<span>Latitude: {position.lat}</span>
+							<span>Longitude: {position.lng}</span>
+						</div>
+						<LocationPicker
+							containerElement={<div style={{ height: '100%' }} />}
+							mapElement={<div style={{ height: '400px' }} />}
+							defaultPosition={position}
+							zoom={18}
+							radius={10}
+							onChange={this.handleLocationChange}
+						/>
 					</div>
 				</div>
 			</div>
 		)
+	}
+
+	renderWhichSearchingType() {
+		const { whichInput } = this.state
+		switch (whichInput) {
+			case SEARCH_BY_NAME:
+				return <input type='text' placeholder='Input location' />
+			case SEARCH_BY_COORDINAT:
+				return (
+					<div>
+						<input type='text' placeholder='Input Latitude' />
+						<input type='text' placeholder='Input Langitude' />
+					</div>
+				)
+			default:
+				break
+		}
 	}
 }
 
