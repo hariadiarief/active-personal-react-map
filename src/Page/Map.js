2b9f719177ce56cb87aaf34383df1ed324d6dcff
hariadiarief <adi.arief77@gmail.com>
2020-04-11 21:00:05 +0700

    styiling input search

--- src/Page/Map.js
+++ src/Page/Map.js
@@ -1,12 +1,9 @@
-import React, { Component } from 'react'
-import { Switch, Route, NavLink } from 'react-router-dom'
+import React, { Component, Fragment } from 'react'
 import Typed from 'react-typed'
+import Geocode from 'react-geocode'
+import LocationPicker from 'react-location-picker'
 
 import { ReactComponent as IconGps } from '../images/banner/gps.svg'
-import InputAddress from '../components/InputAddress'
-import InputCoordinat from '../components/InputCoordinat'
-
-import LocationPicker from 'react-location-picker'
 
 const SEARCH_BY_NAME = 1
 const SEARCH_BY_COORDINAT = 2
@@ -21,11 +18,14 @@ class Map extends Component {
 				lat: 0,
 				lng: 0,
 			},
+			addressinputed: null,
+			addressesSuggestion: [],
 		}
 	}
 
 	componentDidMount() {
 		this.locationInitiation()
+		this.geoCodeFromAddress()
 	}
 
 	locationInitiation() {
@@ -45,8 +45,41 @@ class Map extends Component {
 		this.setState({ position, address })
 	}
 
+	geoCodeFromAddress(value) {
+		Geocode.setApiKey(process.env.REACT_APP_GOOGLE_MAP_API)
+		Geocode.setLanguage('id')
+		Geocode.setRegion('ID')
+
+		Geocode.fromAddress(value).then(
+			(response) => {
+				this.setState({ addressesSuggestion: response.results })
+			},
+			(error) => {
+				console.error(error)
+			}
+		)
+	}
+
+	geoCodeFromLatLng() {
+		Geocode.setApiKey(process.env.REACT_APP_GOOGLE_MAP_API)
+		Geocode.setLanguage('id')
+		Geocode.setRegion('ID')
+
+		const { latitude, longitude } = this.state.merchantUpdate.location
+		Geocode.fromLatLng(latitude, longitude).then(
+			(response) => {
+				const address = response.results[0].formatted_address
+
+				this.setState({ address })
+			},
+			(error) => {
+				console.error(error)
+			}
+		)
+	}
+
 	render() {
-		const { whichInput, position, address } = this.state
+		const { whichInput, position, address, addressinputed, addressesSuggestion } = this.state
 		return (
 			<div className='map'>
 				<div className='map__banner'>
@@ -54,20 +87,37 @@ class Map extends Component {
 					<Typed strings={['Seach', 'Your Location']} typeSpeed={50} backSpeed={25} loop />
 				</div>
 				<div className='map__display'>
-					<div className='navigation map__display__navigation'>
-						<div className='navigations'>
-							<a className={`navigation ${whichInput === SEARCH_BY_NAME && 'navigation--active'}`} onClick={() => this.setState({ whichInput: SEARCH_BY_NAME })}>
-								Pilih Berdasarkan Nama
-							</a>
-							<a className={`navigation ${whichInput === SEARCH_BY_COORDINAT && 'navigation--active'}`} onClick={() => this.setState({ whichInput: SEARCH_BY_COORDINAT })}>
-								Pilih Berdasarkan Koordinat
-							</a>
+					<div className='navigations'>
+						<a className={`navigation ${whichInput === SEARCH_BY_NAME && 'navigation--active'}`} onClick={() => this.setState({ whichInput: SEARCH_BY_NAME })}>
+							Pilih Berdasarkan Nama
+						</a>
+						<a className={`navigation ${whichInput === SEARCH_BY_COORDINAT && 'navigation--active'}`} onClick={() => this.setState({ whichInput: SEARCH_BY_COORDINAT })}>
+							Pilih Berdasarkan Koordinat
+						</a>
+					</div>
+
+					{this.renderWhichSearchingType()}
+
+					<div className='map__display__position'>
+						<div className='map__display__position__address'>
+							<span>Adress :&nbsp;</span>
+							<span>{address}</span>
 						</div>
-						{this.renderWhichSearchingType()}
+						<div className='map__display__position__coordinate'>
+							<div>
+								<span>Latitude :&nbsp;</span>
+								<span>{position.lat}</span>
+							</div>
+							<div>
+								<span>Longitude :&nbsp;</span>
+								<span>{position.lng}</span>
+							</div>
+						</div>
+					</div>
+
+					<div>
 						<div>
-							<span>Address : {address}</span>
-							<span>Latitude: {position.lat}</span>
-							<span>Longitude: {position.lng}</span>
+							<span></span>
 						</div>
 						<LocationPicker
 							containerElement={<div style={{ height: '100%' }} />}
@@ -84,15 +134,24 @@ class Map extends Component {
 	}
 
 	renderWhichSearchingType() {
-		const { whichInput } = this.state
+		const { whichInput, address, addressInputed, position } = this.state
 		switch (whichInput) {
 			case SEARCH_BY_NAME:
-				return <input type='text' placeholder='Input location' />
+				return (
+					<input
+						className='input--address'
+						value={addressInputed}
+						onChange={({ target: { value } }) => this.setState({ addressInputed: value }, this.geoCodeFromAddress(value))}
+						type='text'
+						placeholder='Input location'
+					/>
+				)
+
 			case SEARCH_BY_COORDINAT:
 				return (
-					<div>
-						<input type='text' placeholder='Input Latitude' />
-						<input type='text' placeholder='Input Langitude' />
+					<div className='input--coordinat'>
+						<input type='number' value={position.lat} placeholder='Input Latitude' />
+						<input type='number' value={position.lng} placeholder='Input Langitude' />
 					</div>
 				)
 			default:
